e0a28a3d65bc2612d99c79c0fd8920a20404f337
ElenaKuropatkina <el-vita81@yandex.ru>
2021-09-29 09:05:05 +0300

    lesson1

---
+++ first-web-app/src/main/java/com/elena/FirstServlet.java
@@ -0,0 +1,35 @@
+package com.elena;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class FirstServlet implements Servlet {
+
+    private ServletConfig servletConfig;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.servletConfig = servletConfig;
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().println("<h1>HELLO!!!!</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
